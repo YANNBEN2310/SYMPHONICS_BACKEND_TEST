56ec11c52934daf00bfd09b67775dce8ff30ddf5
Yann BEN ABDERRAHMANE <naravas2015@gmail.com>
2025-12-30 20:17:00 +0100

    fix test_report tester les report_temperature

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,5 +1,10 @@
-def test_get_report(client):
-    response = client.get("/report")
+def test_get_power_report(client):
+    response = client.get("/report/power")
+    assert response.status_code == 200
+    assert isinstance(response.json(), dict)
 
+
+def test_get_temperature_report(client):
+    response = client.get("/report/temperature")
     assert response.status_code == 200
     assert isinstance(response.json(), dict)
