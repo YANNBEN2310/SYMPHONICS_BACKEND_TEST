c9e30ee0eee42222f0532c7ffde0c41e7cbe2ce7
Yann BEN ABDERRAHMANE <naravas2015@gmail.com>
2025-12-30 18:06:51 +0100

    ajouter le router GET/report dans main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.message import router as message_router
 from app.api.send import router as send_router
+from app.api.report import router as report_router
 
 app = FastAPI(
     title="IoT Energy API",
@@ -10,3 +11,4 @@ app = FastAPI(
 
 app.include_router(message_router)
 app.include_router(send_router)
+app.include_router(report_router)
