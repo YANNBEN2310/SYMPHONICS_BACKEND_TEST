97941177cc443d9e6876fc75cef852f54a2e9bb6
Yann BEN ABDERRAHMANE <naravas2015@gmail.com>
2025-12-30 17:43:58 +0100

    ajouter un service pub/sub pour une simulation locale

--- app/configs/config.py
+++ app/configs/config.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+## appeler le répetroire du projet 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings(BaseSettings):
     """
---
+++ app/services/pubsub_service.py
@@ -0,0 +1,23 @@
+import json
+from app.configs.config import settings
+
+
+class PubSubService:
+    """
+    service d'envoi de commandes aux devcies.
+   enlocal, je simule l'envoi pub/sub.
+    """
+
+    def publish_command(self, device_id: str, switch: bool) -> None:
+        message = {
+            "devId": device_id,
+            "switch": switch
+        }
+
+        if settings.ENV == "local":
+            # Simulation locale
+            print(f"[PUBSUB - LOCAL] Topic={settings.PUBSUB_TOPIC} | Message={json.dumps(message)}")
+            return
+
+        # en prod, ici on utiliserait google-cloud-pubsub
+        # publisher.publish(...)
